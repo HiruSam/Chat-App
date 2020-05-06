e7d1cd7c8323492826ac7ba04c94f813d68973e5
[HiruSam] <[hirusamarawickrama824hs@gmail.com]>
2020-05-06 23:40:30 +0530

    Navigation Bar created.

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+// the below components will be created shortly
+import Login from "./components/Login";
+import GroupChat from "./components/GroupChat";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route,Redirect, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <React.Fragment>
+                <Router>
+                    <NavigationBar />
+                </Router>
+                {/*<Switch>*/}
+                {/*    <Redirect exact from="/" to="/login" />*/}
+                {/*    <Route path="/login" component={Login} />*/}
+                {/*</Switch>*/}
+            </React.Fragment>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        );
+    }
 }
-
 export default App;
