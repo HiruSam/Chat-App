2aa45cd9b2f246995973895e7a553deea151c3d0
HiruSam <hirusamarawickrama824hs@gmail.com>
2020-05-20 13:23:58 +0530

    Images & Home page Updated

--- chat-app/src/components/home.js
+++ chat-app/src/components/home.js
@@ -1,8 +1,9 @@
 import React, {Component} from "react";
 import {Carousel} from "react-bootstrap";
-import Caro2 from "../Images/caro1.jpg";
 import Caro1 from "../Images/caro2.jpg";
-import Caro3 from "../Images/caro3.jpg";
+import Caro2 from "../Images/caro4.jpg";
+import Caro3 from "../Images/caro1.jpg";
+import Caro4 from "../Images/caro3.jpg";
 class home extends Component{
 
     render() {
@@ -36,6 +37,14 @@ class home extends Component{
                                         alt="Third slide"
                                     />
 
+                                </Carousel.Item>
+                                <Carousel.Item>
+                                    <img
+                                        className="d-block w-100"
+                                        style={{height:"40rem"}}
+                                        src={Caro4}
+                                        alt="fourth slide"
+                                    />
                                 </Carousel.Item>
                             </Carousel>
                         </Carousel>
