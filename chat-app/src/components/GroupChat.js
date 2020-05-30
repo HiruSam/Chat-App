c98334d175da35748d602a14116770bbff5872c7
HiruSam <hirusamarawickrama824hs@gmail.com>
2020-05-30 22:16:17 +0530

    Updated

--- chat-app/src/components/GroupChat.js
+++ chat-app/src/components/GroupChat.js
@@ -49,12 +49,12 @@ const GroupChat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
+    // const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
 
-        socket = io(ENDPOINT);
+        // socket = io(ENDPOINT);
 
         setRoom(room);
         setName(name);
