7e024bf6fba944cedd77704d10eebb674b035673
HiruSam <hirusamarawickrama824hs@gmail.com>
2020-06-02 22:02:14 +0530

    Edited

--- chat-app/src/components/Chat/Chat.js
+++ chat-app/src/components/Chat/Chat.js
@@ -3,14 +3,14 @@ import Messages from '../Messages/Messages';
 
 import './Chat.css';
 
-let socket;
-
-const Chat = ({ location }) => {
-    const [name, setName] = useState('');
-    const [room, setRoom] = useState('');
-    const [users, setUsers] = useState('');
-    const [message, setMessage] = useState('');
-    const [messages, setMessages] = useState([]);
+// let socket;
+//
+// const Chat = ({ location }) => {
+//     const [name, setName] = useState('');
+//     const [room, setRoom] = useState('');
+//     const [users, setUsers] = useState('');
+//     const [message, setMessage] = useState('');
+//     const [messages, setMessages] = useState([]);
     // const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
 
     // useEffect(() => {
@@ -28,23 +28,53 @@ const Chat = ({ location }) => {
     //     });
     // }, [ENDPOINT, location.search]);
 
-    useEffect(() => {
-        socket.on('message', message => {
-            setMessages(messages => [ ...messages, message ]);
-        });
+    // useEffect(() => {
+    //     socket.on('message', message => {
+    //         setMessages(messages => [ ...messages, message ]);
+    //     });
+    //
+    //     socket.on("roomData", ({ users }) => {
+    //         setUsers(users);
+    //     });
+    // }, []);
+//
+//     return (
+//         <div className="outerContainer">
+//             <div className="container">
+//                 <Messages messages={messages} name={name} />
+//             </div>
+//         </div>
+//     );
+// }
+import React from "react";
+import {Card, CardBody, CardTitle} from "reactstrap";
+class Chat extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <React.Fragment>
+                <div className="my-lg-5">
+                    <Card className="bg-light mb-3 text-center w-50 mx-auto">
+                        <CardBody >
+                            <CardTitle> <h2><strong>  Real Time Chat</strong></h2></CardTitle>
+                            <br/>
 
-        socket.on("roomData", ({ users }) => {
-            setUsers(users);
-        });
-    }, []);
+                            <form>
+                                <div className="form-group text-left">
+                                    <input type="text" name="name" placeholder="Enter Your Friend Name" className="form-control text-center"/>
+                                </div>
+                                <br/>
+                                <input type="submit" value="Enter" className="btn btn-info btn-block mb-3"/>
+                            </form>
+                        </CardBody>
+                    </Card>
+                </div>
+            </React.Fragment>
+        );
 
-    return (
-        <div className="outerContainer">
-            <div className="container">
-                <Messages messages={messages} name={name} />
-            </div>
-        </div>
-    );
+    }
 }
 
 export default Chat;
