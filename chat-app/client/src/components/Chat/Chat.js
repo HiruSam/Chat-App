979a506c6f5942874f5c7c169efd40b85e715a02
HiruSam <hirusamarawickrama824hs@gmail.com>
2020-07-01 07:40:07 +0530

    Change 14

--- chat-app/client/src/components/Chat/Chat.js
+++ chat-app/client/src/components/Chat/Chat.js
@@ -2,6 +2,11 @@ import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from "socket.io-client";
 
+import TextContainer from '../MsgContainer/MsgContainer';
+import Messages from '../Messages/Messages';
+import InfoBar from '../Infor/Infor';
+import Input from '../Input/Input';
+
 import './Chat.css';
 
 let socket;
@@ -12,15 +17,15 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = '';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search);
 
         socket = io(ENDPOINT);
 
-        setName(name);
         setRoom(room);
+        setName(name)
 
         socket.emit('join', { name, room }, (error) => {
             if(error) {
@@ -29,47 +34,34 @@ const Chat = ({ location }) => {
         });
     }, [ENDPOINT, location.search]);
 
+    useEffect(() => {
+        socket.on('message', message => {
+            setMessages(messages => [ ...messages, message ]);
+        });
+
+        socket.on("roomData", ({ users }) => {
+            setUsers(users);
+        });
+    }, []);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    }
+
     return (
         <div className="outerContainer">
             <div className="container">
-                <input
-                value={message}
-                onChange={(event) => setMessage(event.target.value)}
-                onKeyPress={event => event.key === 'Enter' ? sendMessage(event): null}/>
+                <InfoBar room={room} />
+                <Messages messages={messages} name={name} />
+                <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
             </div>
+            <TextContainer users={users}/>
         </div>
     );
 }
 
 export default Chat;
-
-// import React from "react";
-// import './Chat.css';
-// import {Card, CardBody, CardTitle} from "reactstrap";
-// class Chat extends React.Component {
-//     render() {
-//         return(
-//             <React.Fragment>
-//                 <div className="my-lg-5">
-//                     <Card className="bg-light mb-3 text-center w-50 mx-auto">
-//                         <CardBody >
-//                             <CardTitle> <h2><strong>Real Time Chat</strong></h2></CardTitle>
-//                             <br/>
-//
-//                             <form>
-//                                 <div className="form-group text-left">
-//                                     <input type="text" name="name" placeholder="Enter Your Friend Name" className="form-control text-center"/>
-//                                 </div>
-//                                 <br/>
-//                                 <input type="submit" value="Enter" className="btn btn-info btn-block mb-3"/>
-//                             </form>
-//                         </CardBody>
-//                     </Card>
-//                 </div>
-//             </React.Fragment>
-//         );
-//
-//     }
-// }
-
-// export default Chat;
